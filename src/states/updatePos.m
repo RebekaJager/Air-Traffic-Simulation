d996d81f9f5eb4c14e7fefc831cfcd67a1212fce
RebekaJager <rjager@edu.bme.hu>
2026-09-17 09:02:05 +0200

    Update updatePos.m
    Performance improvement

--- src/states/updatePos.m
+++ src/states/updatePos.m
@@ -14,30 +14,23 @@ function [D] = updatePos(D, D_refreshed)
 %   Output Arguments
 %      * D as structure, structure of traffic data, where positions flaged
 %         as outside are to be updated from D2.
-
-if isfield(D_refreshed, 'inside') == 0
-    error('The inside field is needed in D_refreshed.')
-end
-
-D_inside = D([D(:).inside] == 1);
-
-for i = length(D_refreshed) : -1 : 1
-     idx = find(strcmp({D_inside(:).ICAO24}, D_refreshed(i).ICAO24));
-     if isempty(idx)
-        D_refreshed(i).update = 1;
-     else
-         D_refreshed(i).update = 0;
-     end
-end
-D_refreshed = D_refreshed([D_refreshed(:).update] == 1);
-D_refreshed = rmfield(D_refreshed, 'update');
-
-Fields = intersect(fieldnames(D_refreshed), fieldnames(D_inside));
-
-
-D_inside_connect = rmfield(D_inside, setdiff(fieldnames(D_inside), Fields));
-D_refreshed_connect = rmfield(D_refreshed, setdiff(fieldnames(D_refreshed), Fields));
-
-D = [D_inside_connect; D_refreshed_connect];
-
+    
+    if ~isfield(D_refreshed, 'inside')
+        error('The inside field is needed in D_refreshed.');
+    end
+    
+    D_inside = D([D(:).inside] == 1);
+    
+    icao_inside = {D_inside.ICAO24};
+    icao_refreshed = {D_refreshed.ICAO24};
+    
+    to_update = ~ismember(icao_refreshed, icao_inside);
+    D_refreshed = D_refreshed(to_update);
+    
+    Fields = intersect(fieldnames(D_refreshed), fieldnames(D_inside));
+    
+    D_inside_connect = rmfield(D_inside, setdiff(fieldnames(D_inside), Fields));
+    D_refreshed_connect = rmfield(D_refreshed, setdiff(fieldnames(D_refreshed), Fields));
+    
+    D = [D_inside_connect; D_refreshed_connect];
 end
